2e22c5057011950a151c15b3717c430ab1693fca
Shahid <shahidkamal08@gmail.com>
2025-04-21 13:04:53 +0530

    solution added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { AuthProvider } from "@/hooks/useAuth";
 import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log("done");
   return (
     <AuthProvider>
       <Component {...pageProps} />
